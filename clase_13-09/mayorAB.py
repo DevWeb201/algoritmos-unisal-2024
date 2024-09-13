7cfe08693f553b3bddc3de73c57b6c53f4f6334e
Adrianlrx <adrianlrx@gmail.com>
2024-09-13 19:26:12 -0400

    comentarios

--- clase_13-09/mayorAB.py
+++ clase_13-09/mayorAB.py
@@ -1,7 +1,9 @@
+# Titulo del scritp
 print("Algoritmo 1")
+# ingreso nro a y b
 a = int(input("Introduzca un número: "))
 b = int(input("Introduzca un número: "))
-
+# comparación
 if a>b:
     print(f"el número {a} es mayor a {b}")
 else:
