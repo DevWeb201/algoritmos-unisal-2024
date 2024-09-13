fca034d807695d82bb17af6bcac67de7b87463e9
Adrianlrx <adrianlrx@gmail.com>
2024-09-13 18:22:05 -0400

    primer ejercicio

---
+++ mayorAB.py
@@ -0,0 +1,8 @@
+a = int(input("Introduzca un número: "))
+b = int(input("Introduzca un número: "))
+
+if a>b:
+    print(f"el número {a} es mayor a {b}")
+else:
+    print(f"el número {b} es mayor a {a}")
+    
