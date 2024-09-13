f585e57f1235cc17885c6ba7f2e3ca7931794bb6
Adrianlrx <adrianlrx@gmail.com>
2024-09-13 19:27:59 -0400

    comilla

--- clase_13-09/mayorAB.py
+++ clase_13-09/mayorAB.py
@@ -2,7 +2,7 @@
 print("Algoritmo 1")
 # ingreso nro a y b
 a = int(input("Introduzca un número: "))
-b = int(input("Introduzca un número: "))
+b = int(input('Introduzca un número: '))
 # comparación
 if a>b:
     print(f"el número {a} es mayor a {b}")
