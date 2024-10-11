5c78f46121ceac3b8c6fdfd240827a83bed470f0
Adrianlrx <adrianlrx@gmail.com>
2024-10-11 19:22:41 -0300

    Clase 11-10-2024

---
+++ clase_11-10/funciones.py
@@ -0,0 +1,12 @@
+"""
+Calcular en area de un cuadrado con una función
+"""
+def area_cuadrado(l):
+    p = l*4
+    a = l**2
+    print(f"el perimetro del cuadrado es: {p}")
+    print(f"el area del cuadrado es: {a:.2f}")
+# sale de la funcion
+
+l = float(input("ingrese el valor de lado del cuadrado: "))
+area_cuadrado(l)
