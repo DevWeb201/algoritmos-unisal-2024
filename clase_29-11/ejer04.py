21a8625506b6f24c14809702bff92a177e3d30d6
Adrianlrx <adrianlrx@gmail.com>
2024-11-29 20:40:27 -0300

    Clase 29-11

--- clase_29-11/ejer04.py
+++ clase_29-11/ejer04.py
@@ -7,3 +7,11 @@ def perimetro_circulo(radio):
 # Función para calcular el área de un círculo
 def area_circulo(radio):
     return math.pi * radio ** 2
+
+def main():
+    radio = float(input("Ingrese el radio del circulo: "))
+    print(f"El perimetro del circulo es: {perimetro_circulo(radio):.2f}")
+    print(f"El area del circulo es: {area_circulo(radio):.2f}")
+
+if __name__ == "__main__":
+    main()
