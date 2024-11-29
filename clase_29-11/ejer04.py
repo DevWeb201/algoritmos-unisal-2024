61706a41500ffc61632419e590c08ff8cb7c0cb7
Adrianlrx <adrianlrx@gmail.com>
2024-11-29 19:21:41 -0300

    Clase 29-11

---
+++ clase_29-11/ejer04.py
@@ -0,0 +1,9 @@
+import math
+
+# Función para calcular el perímetro de un círculo
+def perimetro_circulo(radio):
+    return 2 * math.pi * radio
+
+# Función para calcular el área de un círculo
+def area_circulo(radio):
+    return math.pi * radio ** 2
